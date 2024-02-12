f2a69dea0d8509bf348b5da4a8e387a1b2a71faf
IA7308 <naziqnaz13@gmail.com>
2024-02-12 22:44:27 +0700

    perbaikan

--- app/Http/Controllers/NeracaController.php
+++ app/Http/Controllers/NeracaController.php
@@ -13,7 +13,8 @@ class NeracaController extends Controller
         // $perPage = strtolower(request('pagination', 'all'));
         session(['paginate' => false]);
         $dataCOA = COA::all();
-        $dataPenyesuaian = penyesuaian::all();
+        $dataPenyesuaianDebit = penyesuaian::all()->keyBy('akunD');
+        $dataPenyesuaianKredit = penyesuaian::all()->keyBy('akunK');
         $data = [];
         foreach($dataCOA as $dc){
             $neraca = new Neraca;
@@ -25,45 +26,50 @@ class NeracaController extends Controller
                 $neraca->rpPD = 0;
                 $neraca->rpPK = 0;
                 $neraca->backgroundClass = 'table-info';
-                foreach($dataPenyesuaian as $dp){
-                    if($neraca->nama_akun == $dp->akunD){
-                        if($neraca->rpD == 0){
-                            $neraca->rpPD = $dp->rpD;
-                        }else{
-                            $neraca->rpPD = $neraca->rpPD+$dp->rpD;
-                        }
-                        
-                        $neraca->rpPK = 0;
-                        $calc = $neraca->rpD + $neraca->rpPD;
-                        if($calc<0){
-                            $neraca->SaldoPenyesuaianP = $calc;
-                            $neraca->SaldoPenyesuaianN = 0; 
-                        }elseif($calc>=0){
-                            $neraca->SaldoPenyesuaianP = 0;
-                            $neraca->SaldoPenyesuaianN = $calc;
-                        }
-                    }elseif($neraca->nama_akun == $dp->akunK){
-                        $neraca->rpPD = 0;
-                        if($neraca->rpPK==0){
-                            $neraca->rpPK = $dp->rpK;
-                        }else{
-                            $neraca->rpPK = $neraca->rpPK-$dp->rpK;
-                        }
-                        
-                        $calc = $neraca->rpK + $neraca->rpPK;
-                        if($calc<0){
-                            $neraca->SaldoPenyesuaianP = $calc;
-                            $neraca->SaldoPenyesuaianN = 0; 
-                        }elseif($calc>=0){
-                            $neraca->SaldoPenyesuaianP = 0;
-                            $neraca->SaldoPenyesuaianN = $calc;
-                        }
+                if($dataPenyesuaianDebit->has($neraca->nama_akun)){
+                    $penyesuaian = $dataPenyesuaianDebit[$neraca->nama_akun];
+                    if($neraca->rpPD == 0){
+                        $neraca->rpPD = $penyesuaian->rpD;
                     }else{
-                        $neraca->rpPD = 0;
-                        $neraca->rpPK = 0;
-                        $neraca->SaldoPenyesuaianP = 0;
-                        $neraca->SaldoPenyesuaianN = 0;
+                        $neraca->rpPD = $neraca->rpPD+$penyesuaian->rpD;
                     }
+                    
+                    $neraca->rpPK = 0;
+                    $calc = $neraca->rpD + $neraca->rpPD;
+                    if($calc<0){
+                        $neraca->SaldoPenyesuaianP = $calc;
+                        $neraca->SaldoPenyesuaianN = 0; 
+                    }elseif($calc>=0){
+                        $neraca->SaldoPenyesuaianP = 0;
+                        $neraca->SaldoPenyesuaianN = $calc;
+                    }
+                }else{
+                    $neraca->rpPD = 0;
+                    $neraca->rpPK = 0;
+                    $neraca->SaldoPenyesuaianP = 0;
+                    $neraca->SaldoPenyesuaianN = 0;
+                }
+                if($dataPenyesuaianKredit->has($neraca->nama_akun)){
+                    $penyesuaianK = $dataPenyesuaianKredit[$neraca->nama_akun];
+                    if($neraca->rpPK==0){
+                        $neraca->rpPK = $penyesuaianK->rpK;
+                    }else{
+                        $neraca->rpPK = $neraca->rpPK-$penyesuaianK->rpK;
+                    }
+                    
+                    $calc = $neraca->rpK + $neraca->rpPK;
+                    if($calc<0){
+                        $neraca->SaldoPenyesuaianP = $calc;
+                        $neraca->SaldoPenyesuaianN = 0; 
+                    }elseif($calc>=0){
+                        $neraca->SaldoPenyesuaianP = 0;
+                        $neraca->SaldoPenyesuaianN = $calc;
+                    }
+                }else{
+                    $neraca->rpPD = 0;
+                    $neraca->rpPK = 0;
+                    $neraca->SaldoPenyesuaianP = 0;
+                    $neraca->SaldoPenyesuaianN = 0;
                 }
                 if($neraca->SaldoPenyesuaianN != 0 || $neraca->SaldoPenyesuaianP != 0){
                     if($dc->jenis_akun == 'A REAL'){
@@ -98,42 +104,22 @@ class NeracaController extends Controller
                 $neraca->rpK = 0;
                 $neraca->rpPD = 0;
                 $neraca->rpPK = 0;
-                foreach($dataPenyesuaian as $dp){
-                    if($neraca->nama_akun == $dp->akunD){
-                        if($neraca->rpD == 0){
-                            $neraca->rpPD = $dp->rpD;
-                        }else{
-                            $neraca->rpPD = $neraca->rpPD+$dp->rpD;
-                        }
-                        $neraca->rpPK = 0;                       
-                        $calc = $neraca->rpD + $neraca->rpPD;
-                        if($calc<0){
-                            $neraca->SaldoPenyesuaianP = 0;
-                            $neraca->SaldoPenyesuaianN = $calc; 
-                        }elseif($calc>=0){
-                            $neraca->SaldoPenyesuaianP = $calc;
-                            $neraca->SaldoPenyesuaianN = 0;
-                        }
-                    }elseif($neraca->nama_akun == $dp->akunK){
-                        $neraca->rpPD = 0;
-                        if($neraca->rpPK==0){
-                            $neraca->rpPK = $dp->rpK;
-                        }else{
-                            $neraca->rpPK = $neraca->rpPK+$dp->rpK;
-                        }
-                        $calc = $neraca->rpK - $neraca->rpPK;
-                        if($calc<0){
-                            $neraca->SaldoPenyesuaianP = 0;
-                            $neraca->SaldoPenyesuaianN = $calc; 
-                        }elseif($calc>=0){
-                            $neraca->SaldoPenyesuaianP = $calc;
-                            $neraca->SaldoPenyesuaianN = 0;
-                        }
+                if($dataPenyesuaianDebit->has($neraca->nama_akun)){
+                    $penyesuaian = $dataPenyesuaianDebit[$neraca->nama_akun];
+                    if($neraca->rpPD == 0){
+                        $neraca->rpPD = $penyesuaian->rpD;
                     }else{
-                        $neraca->rpPD = 0;
-                        $neraca->rpPK = 0;
-                        $neraca->SaldoPenyesuaianP = $neraca->rpD;
-                        $neraca->SaldoPenyesuaianN = 0;
+                        $neraca->rpPD = $neraca->rpPD+$penyesuaian->rpD;
+                    }
+                    
+                    $neraca->rpPK = 0;
+                    $calc = $neraca->rpD + $neraca->rpPD;
+                    if($calc<0){
+                        $neraca->SaldoPenyesuaianP = $calc;
+                        $neraca->SaldoPenyesuaianN = 0; 
+                    }elseif($calc>=0){
+                        $neraca->SaldoPenyesuaianP = 0;
+                        $neraca->SaldoPenyesuaianN = $calc;
                     }
                 }
                 if($neraca->SaldoPenyesuaianN != 0 || $neraca->SaldoPenyesuaianP != 0){
@@ -168,42 +154,21 @@ class NeracaController extends Controller
                 $neraca->rpK = $dc->jumlah_saldo;
                 $neraca->rpPD = 0;
                 $neraca->rpPK = 0;
-                foreach($dataPenyesuaian as $dp){
-                    if($neraca->nama_akun == $dp->akunD){
-                        if($neraca->rpD == 0){
-                            $neraca->rpPD = $dp->rpD;
-                        }else{
-                            $neraca->rpPD = $neraca->rpPD+$dp->rpD;
-                        }
-                        $neraca->rpPK = 0;
-                        $calc = $neraca->rpD + $neraca->rpPD;
-                        if($calc<0){
-                            $neraca->SaldoPenyesuaianP = 0;
-                            $neraca->SaldoPenyesuaianN = $calc; 
-                        }elseif($calc>=0){
-                            $neraca->SaldoPenyesuaianP = $calc;
-                            $neraca->SaldoPenyesuaianN = 0;
-                        }
-                    }elseif($neraca->nama_akun == $dp->akunK){
-                        $neraca->rpPD = 0;
-                        if($neraca->rpPK==0){
-                            $neraca->rpPK = $dp->rpK;
-                        }else{
-                            $neraca->rpPK = $neraca->rpPK+$dp->rpK;
-                        }
-                        $calc = $neraca->rpK - $neraca->rpPK;
-                        if($calc<0){
-                            $neraca->SaldoPenyesuaianP = 0;
-                            $neraca->SaldoPenyesuaianN = $calc; 
-                        }elseif($calc>=0){
-                            $neraca->SaldoPenyesuaianP = $calc;
-                            $neraca->SaldoPenyesuaianN = 0;
-                        }
+                if($dataPenyesuaianKredit->has($neraca->nama_akun)){
+                    $penyesuaianK = $dataPenyesuaianKredit[$neraca->nama_akun];
+                    if($neraca->rpPK==0){
+                        $neraca->rpPK = $penyesuaianK->rpK;
                     }else{
-                        $neraca->rpPD = 0;
-                        $neraca->rpPK = 0;
+                        $neraca->rpPK = $neraca->rpPK-$penyesuaianK->rpK;
+                    }
+                    
+                    $calc = $neraca->rpK - $neraca->rpPK;
+                    if($calc<0){
+                        $neraca->SaldoPenyesuaianP = $calc;
+                        $neraca->SaldoPenyesuaianN = 0; 
+                    }elseif($calc>=0){
                         $neraca->SaldoPenyesuaianP = 0;
-                        $neraca->SaldoPenyesuaianN = $neraca->rpK;
+                        $neraca->SaldoPenyesuaianN = $calc;
                     }
                 }
                 if($neraca->SaldoPenyesuaianN != 0 || $neraca->SaldoPenyesuaianP != 0){
@@ -237,5 +202,128 @@ class NeracaController extends Controller
         return view('Neraca_Jalur', ['data' => $data]);
     }
 
-    
+    // public function isiPenyesuaian($id,$neraca){
+    //     $akun = COA::find($id);
+    //     $dataPenyesuaian = penyesuaian::all()->toArray();
+        // if($akun->keterangan == 'Header' || $akun->keterangan == 'Jumlah'){
+        //     for($i = 0; $i < count($dataPenyesuaian); $i++){
+        //         if($neraca->nama_akun == $dataPenyesuaian[$i]['akunD']){
+                    // if($neraca->rpD == 0){
+                    //     $neraca->rpPD = $dataPenyesuaian[$i]['rpD'];
+                    // }else{
+                    //     $neraca->rpPD = $neraca->rpPD+$dataPenyesuaian[$i]['rpD'];
+                    // }
+                    
+                    // $neraca->rpPK = 0;
+                    // $calc = $neraca->rpD + $neraca->rpPD;
+                    // if($calc<0){
+                    //     $neraca->SaldoPenyesuaianP = $calc;
+                    //     $neraca->SaldoPenyesuaianN = 0; 
+                    // }elseif($calc>=0){
+                    //     $neraca->SaldoPenyesuaianP = 0;
+                    //     $neraca->SaldoPenyesuaianN = $calc;
+                    // }
+        //         }elseif($neraca->nama_akun == $dataPenyesuaian[$i]['akunK']){
+        //             $neraca->rpPD = 0;
+                    // if($neraca->rpPK==0){
+                    //     $neraca->rpPK = $dataPenyesuaian[$i]['rpK'];
+                    // }else{
+                    //     $neraca->rpPK = $neraca->rpPK-$dataPenyesuaian[$i]['rpK'];
+                    // }
+                    
+                    // $calc = $neraca->rpK + $neraca->rpPK;
+                    // if($calc<0){
+                    //     $neraca->SaldoPenyesuaianP = $calc;
+                    //     $neraca->SaldoPenyesuaianN = 0; 
+                    // }elseif($calc>=0){
+                    //     $neraca->SaldoPenyesuaianP = 0;
+                    //     $neraca->SaldoPenyesuaianN = $calc;
+                    // }
+        //         }else{
+        //             $neraca->rpPD = 0;
+        //             $neraca->rpPK = 0;
+        //             $neraca->SaldoPenyesuaianP = 0;
+        //             $neraca->SaldoPenyesuaianN = 0;
+        //         }
+        //     }
+        // }elseif($akun->keterangan == 'Akun, Debit'){
+    //         for($i = 0; $i < count($dataPenyesuaian); $i++){
+    //             if($akun->Nama_akun == $dataPenyesuaian[$i]['akunD']){
+    //                 if($neraca->rpD == 0){
+    //                     $neraca->rpPD = $dataPenyesuaian[$i]['rpD'];
+    //                 }else{
+    //                     $neraca->rpPD = $neraca->rpPD+$dataPenyesuaian[$i]['rpD'];
+    //                 }
+    //                 $neraca->rpPK = 0;                       
+    //                 $calc = $neraca->rpD + $neraca->rpPD;
+    //                 if($calc<0){
+    //                     $neraca->SaldoPenyesuaianP = 0;
+    //                     $neraca->SaldoPenyesuaianN = $calc; 
+    //                 }elseif($calc>=0){
+    //                     $neraca->SaldoPenyesuaianP = $calc;
+    //                     $neraca->SaldoPenyesuaianN = 0;
+    //                 }
+    //             }elseif($neraca->nama_akun == $dataPenyesuaian[$i]['akunK']){
+    //                 $neraca->rpPD = 0;
+    //                 if($neraca->rpPK==0){
+    //                     $neraca->rpPK = $dataPenyesuaian[$i]['rpK'];
+    //                 }else{
+    //                     $neraca->rpPK = $neraca->rpPK+$dataPenyesuaian[$i]['rpK'];
+    //                 }
+    //                 $calc = $neraca->rpK - $neraca->rpPK;
+    //                 if($calc<0){
+    //                     $neraca->SaldoPenyesuaianP = 0;
+    //                     $neraca->SaldoPenyesuaianN = $calc; 
+    //                 }elseif($calc>=0){
+    //                     $neraca->SaldoPenyesuaianP = $calc;
+    //                     $neraca->SaldoPenyesuaianN = 0;
+    //                 }
+    //             }else{
+    //                 $neraca->rpPD = 0;
+    //                 $neraca->rpPK = 0;
+    //                 $neraca->SaldoPenyesuaianP = $neraca->rpD;
+    //                 $neraca->SaldoPenyesuaianN = 0;
+    //             }
+    //         }
+    //     }else{
+    //         for($i = 0; $i < count($dataPenyesuaian); $i++){
+    //             if($neraca->nama_akun == $dataPenyesuaian[$i]['akunD']){
+    //                 if($neraca->rpD == 0){
+    //                     $neraca->rpPD = $dataPenyesuaian[$i]['rpD'];
+    //                 }else{
+    //                     $neraca->rpPD = $neraca->rpPD+$dataPenyesuaian[$i]['rpD'];
+    //                 }
+    //                 $neraca->rpPK = 0;
+    //                 $calc = $neraca->rpD + $neraca->rpPD;
+    //                 if($calc<0){
+    //                     $neraca->SaldoPenyesuaianP = 0;
+    //                     $neraca->SaldoPenyesuaianN = $calc; 
+    //                 }elseif($calc>=0){
+    //                     $neraca->SaldoPenyesuaianP = $calc;
+    //                     $neraca->SaldoPenyesuaianN = 0;
+    //                 }
+    //             }elseif($neraca->nama_akun == $dataPenyesuaian[$i]['akunK']){
+    //                 $neraca->rpPD = 0;
+    //                 if($neraca->rpPK==0){
+    //                     $neraca->rpPK = $dataPenyesuaian[$i]['rpK'];
+    //                 }else{
+    //                     $neraca->rpPK = $neraca->rpPK+$dataPenyesuaian[$i]['rpK'];
+    //                 }
+    //                 $calc = $neraca->rpK - $neraca->rpPK;
+    //                 if($calc<0){
+    //                     $neraca->SaldoPenyesuaianP = 0;
+    //                     $neraca->SaldoPenyesuaianN = $calc; 
+    //                 }elseif($calc>=0){
+    //                     $neraca->SaldoPenyesuaianP = $calc;
+    //                     $neraca->SaldoPenyesuaianN = 0;
+    //                 }
+    //             }else{
+    //                 $neraca->rpPD = 0;
+    //                 $neraca->rpPK = 0;
+    //                 $neraca->SaldoPenyesuaianP = 0;
+    //                 $neraca->SaldoPenyesuaianN = $neraca->rpK;
+    //             }
+    //         }
+    //     }
+    // }    
 }
